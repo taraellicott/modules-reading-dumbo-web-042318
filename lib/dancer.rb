1d1f079d2ec3a73dc7cbc5e0e0b7ebef7f175dd4
github username <github email address>
2018-05-06 20:36:36 -0400

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,9 @@
+require_relative './fancy_dance.rb'
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
+  extend MetaDancing
 include Dance
 
 attr_accessor :name
@@ -9,4 +11,5 @@ attr_accessor :name
 def initialize(name)
   @name = name
 end
+
 end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,9 @@
+require_relative './fancy_dance.rb'
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
+  extend MetaDancing
 include Dance
 
 attr_accessor :name
